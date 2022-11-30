fcbbd7e800a4cd0e4731a0814d37314fad3a8619
amolmane7471 <maneamol3233@gmail.com>
2022-11-30 22:24:43 +0530

    [Amol]: added to display bridgelabz logo using property display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/img/BL_logo_square_jpg.jpg'
 
 class App extends React.Component {
   constructor(){
@@ -12,6 +13,8 @@ class App extends React.Component {
   return (
       <div>
           <h1>{this.state.title}</h1>
+          <img src={logo}
+            alt="The Bridgelabz Logo: a Bridgelabz To Employment Through Lab Works" />
       </div>
     );
   }
