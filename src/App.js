161d4cd708d95b15400a88bd3e4d5ef829dcb790
amolmane7471 <maneamol3233@gmail.com>
2022-11-30 22:40:49 +0530

    [Amol]: added ability to launch bridgelabz site on clicking bridgelabz logo

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './App.css';
 import logo from './assets/img/BL_logo_square_jpg.jpg'
 
 class App extends React.Component {
+  url='https://www.bridgelabz.com'
+
   constructor(){
     super()
     this.state = {
       title: 'Hello From Bridgelabz'
     }
   }
+
+//onclick function
+  onClick = (event) =>{
+    console.log("Button is clicked!",event);
+    window.open(this.url,"_blank");
+}
+
   render(){
   return (
       <div>
           <h1>{this.state.title}</h1>
-          <img src={logo}
+          <img src={logo} onClick={this.onClick}
             alt="The Bridgelabz Logo: a Bridgelabz To Employment Through Lab Works" />
       </div>
     );
